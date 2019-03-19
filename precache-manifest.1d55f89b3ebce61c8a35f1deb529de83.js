882af4cfd6cec2b9dde4800aafd943ac9f8677d2
rot1024 <aayhrot@gmail.com>
2019-03-19 15:55:55 +0900

    Updates

---
+++ precache-manifest.1d55f89b3ebce61c8a35f1deb529de83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "952fdb8492ab6e5db750924fe2fc1491",
+    "url": "./static/media/splash.952fdb84.png"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "0434598c1a0e3573a56f",
+    "url": "./static/js/main.551d7068.chunk.js"
+  },
+  {
+    "revision": "e6e92b9363fd48f02abf",
+    "url": "./static/js/2.244ebe41.chunk.js"
+  },
+  {
+    "revision": "0434598c1a0e3573a56f",
+    "url": "./static/css/main.d4bf85d0.chunk.css"
+  },
+  {
+    "revision": "58dfc99a52c11cb3c2748187903d2a16",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b9809ccf87d694203a98f64dcab8ef24.js"
+  "./precache-manifest.1d55f89b3ebce61c8a35f1deb529de83.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
