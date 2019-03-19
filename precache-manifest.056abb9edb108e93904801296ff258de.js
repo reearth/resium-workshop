28dc39294186946f239b2b81af66f58b81c8f001
rot1024 <aayhrot@gmail.com>
2019-03-19 14:35:18 +0900

    init gh-pages

---
+++ precache-manifest.056abb9edb108e93904801296ff258de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "952fdb8492ab6e5db750924fe2fc1491",
+    "url": "/static/media/splash.952fdb84.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "4824884c99341bfced98",
+    "url": "/static/js/main.242f44e7.chunk.js"
+  },
+  {
+    "revision": "f60ac36d2c510c1b7999",
+    "url": "/static/js/2.451538ec.chunk.js"
+  },
+  {
+    "revision": "4824884c99341bfced98",
+    "url": "/static/css/main.c2332dba.chunk.css"
+  },
+  {
+    "revision": "00875658201e226dac393cdd620373c9",
+    "url": "/index.html"
+  }
+];
